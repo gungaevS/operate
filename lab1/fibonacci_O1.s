8825f8152f04b5573b041e76dd100fefcd344478
gungaevS <gungaevsodnom@gmail.com>
2026-05-27 08:49:14 +0800

    Update fibonacci_O1.s

--- lab1/fibonacci_O1.s
+++ lab1/fibonacci_O1.s
@@ -5,23 +5,23 @@
 fib_iter:
 .LFB22:
 	.cfi_startproc
-	cmpl	$1, %edi
-	jle	.L6
-	addl	$1, %edi
-	movl	$2, %edx
-	movl	$1, %eax
-	movl	$0, %ecx
+	cmpl	$1, %edi              ; Сравнить n с 1
+	jle	.L6                    ; Если n <= 1, вернуть n
+	addl	$1, %edi              ; Увеличить n на 1 (для условия цикла: i != n+1)
+	movl	$2, %edx              ; i = 2 (счётчик цикла)
+	movl	$1, %eax              ; b = 1 (текущее число Фибоначчи, будет в rax)
+	movl	$0, %ecx              ; a = 0 (предыдущее число, будет в rcx)
 	.p2align 4
-.L4:
-	movq	%rax, %rsi
-	addq	%rcx, %rax
-	addl	$1, %edx
-	movq	%rsi, %rcx
-	cmpl	%edi, %edx
-	jne	.L4
-	ret
-.L6:
-	movslq	%edi, %rax
+.L4:                                ; Начало тела цикла
+	movq	%rax, %rsi            ; tmp = b (сохраняем старое b)
+	addq	%rcx, %rax            ; b = b + a (новое число Фибоначчи)
+	addl	$1, %edx              ; i++ (увеличиваем счётчик)
+	movq	%rsi, %rcx            ; a = tmp (старое b становится новым a)
+	cmpl	%edi, %edx            ; Сравнить i с n+1
+	jne	.L4                    ; Если не равны, продолжить цикл
+	ret                            ; Вернуть b (оно в rax)
+.L6:                                ; Случай n <= 1
+	movslq	%edi, %rax            ; Расширить n до 64 бит и вернуть
 	ret
 	.cfi_endproc
 .LFE22:
@@ -31,29 +31,29 @@ fib_iter:
 fib_rec:
 .LFB23:
 	.cfi_startproc
-	pushq	%r14
+	pushq	%r14                  ; Сохранить регистры, которые будут испорчены
 	.cfi_def_cfa_offset 16
 	.cfi_offset 14, -16
 	pushq	%rbx
 	.cfi_def_cfa_offset 24
 	.cfi_offset 3, -24
-	subq	$8, %rsp
+	subq	$8, %rsp              ; Выровнять стек
 	.cfi_def_cfa_offset 32
-	movl	%edi, %ebx
-	movslq	%edi, %rax
-	cmpl	$1, %edi
-	jle	.L7
-	leal	-1(%rdi), %edi
-	call	fib_rec
-	movq	%rax, %r14
-	leal	-2(%rbx), %edi
-	call	fib_rec
-	addq	%rax, %r14
-	movq	%r14, %rax
+	movl	%edi, %ebx            ; Сохранить n в ebx
+	movslq	%edi, %rax            ; Расширить n до 64 бит
+	cmpl	$1, %edi              ; Сравнить n с 1
+	jle	.L7                    ; Если n <= 1, вернуть n
+	leal	-1(%rdi), %edi        ; edi = n-1
+	call	fib_rec               ; Вызвать fib_rec(n-1)
+	movq	%rax, %r14            ; Сохранить результат в r14
+	leal	-2(%rbx), %edi        ; edi = n-2
+	call	fib_rec               ; Вызвать fib_rec(n-2)
+	addq	%rax, %r14            ; Сложить fib_rec(n-1) + fib_rec(n-2)
+	movq	%r14, %rax            ; Результат в rax
 .L7:
-	addq	$8, %rsp
+	addq	$8, %rsp              ; Восстановить стек
 	.cfi_def_cfa_offset 24
-	popq	%rbx
+	popq	%rbx                  ; Восстановить регистры
 	.cfi_def_cfa_offset 16
 	popq	%r14
 	.cfi_def_cfa_offset 8
@@ -63,64 +63,65 @@ fib_rec:
 	.size	fib_rec, .-fib_rec
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.string	"Usage: %s <n> <method>\n"
+	.string	"Usage: %s <n> <method>\n"    ; Строка справки
 .LC1:
-	.string	"Fibonacci(%d) = %llu\n"
+	.string	"Fibonacci(%d) = %llu\n"      ; Строка вывода результата
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB24:
 	.cfi_startproc
-	pushq	%r14
+	pushq	%r14                  ; Сохранить регистры
 	.cfi_def_cfa_offset 16
 	.cfi_offset 14, -16
 	pushq	%rbx
 	.cfi_def_cfa_offset 24
 	.cfi_offset 3, -24
-	subq	$8, %rsp
+	subq	$8, %rsp              ; Выровнять стек
 	.cfi_def_cfa_offset 32
-	movq	%rsi, %rbx
-	cmpl	$3, %edi
-	je	.L12
-	movq	(%rsi), %rdx
-	leaq	.LC0(%rip), %rsi
-	movq	stderr(%rip), %rdi
+	movq	%rsi, %rbx            ; Сохранить argv в rbx
+	cmpl	$3, %edi              ; Сравнить argc с 3
+	je	.L12                   ; Если == 3, перейти к обработке
+	; Вывод подсказки при неправильном количестве аргументов
+	movq	(%rsi), %rdx          ; argv[0] в rdx (3-й аргумент)
+	leaq	.LC0(%rip), %rsi      ; Строка формата
+	movq	stderr(%rip), %rdi    ; stderr (1-й аргумент)
 	movl	$0, %eax
-	call	fprintf@PLT
-	movl	$1, %eax
+	call	fprintf@PLT           ; Вывести сообщение об ошибке
+	movl	$1, %eax              ; Вернуть 1
 .L11:
-	addq	$8, %rsp
+	addq	$8, %rsp              ; Восстановить стек
 	.cfi_remember_state
 	.cfi_def_cfa_offset 24
-	popq	%rbx
+	popq	%rbx                  ; Восстановить регистры
 	.cfi_def_cfa_offset 16
 	popq	%r14
 	.cfi_def_cfa_offset 8
 	ret
 .L12:
 	.cfi_restore_state
-	movq	8(%rsi), %rdi
-	movl	$10, %edx
-	movl	$0, %esi
-	call	__isoc23_strtol@PLT
-	movl	%eax, %edi
-	movl	%eax, %r14d
-	movq	16(%rbx), %rax
-	cmpb	$105, (%rax)
-	jne	.L14
-	call	fib_iter
-	movq	%rax, %rdx
+	movq	8(%rsi), %rdi         ; argv[1] в rdi (для strtol)
+	movl	$10, %edx             ; Основание системы счисления = 10
+	movl	$0, %esi              ; Конечный указатель не нужен
+	call	__isoc23_strtol@PLT   ; Преобразовать строку в число
+	movl	%eax, %edi            ; Результат (n) в edi
+	movl	%eax, %r14d           ; Сохранить n в r14d
+	movq	16(%rbx), %rax        ; argv[2] в rax
+	cmpb	$105, (%rax)          ; Сравнить первый символ с 'i'
+	jne	.L14                   ; Если не 'i', вызвать рекурсивную версию
+	call	fib_iter              ; Вызвать итеративное вычисление
+	movq	%rax, %rdx            ; Результат в rdx
 .L15:
-	movl	%r14d, %esi
-	leaq	.LC1(%rip), %rdi
+	movl	%r14d, %esi           ; n во 2-й аргумент printf
+	leaq	.LC1(%rip), %rdi      ; Строка формата в 1-й аргумент
 	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
+	call	printf@PLT           ; Вывести результат
+	movl	$0, %eax              ; Вернуть 0
 	jmp	.L11
 .L14:
-	call	fib_rec
-	movq	%rax, %rdx
+	call	fib_rec               ; Вызвать рекурсивное вычисление
+	movq	%rax, %rdx            ; Результат в rdx
 	jmp	.L15
 	.cfi_endproc
 .LFE24:
